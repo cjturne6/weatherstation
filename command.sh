c8d5b046d5b0cb78b8140d154d2fd7b69b44538a
cjturne6 <55654880+cjturne6@users.noreply.github.com>
2021-04-23 14:16:39 -0500

    misc

--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-python3 environmental_monitor.py --endpoint a16860lqrlz0ti-ats.iot.us-east-1.amazonaws.com --rootCA AmazonRootCA1.pem --cert certificate.pem.crt --key private.pem.key --thingName RaspiZeroWeather --clientId RaspiZeroWeather
+python3 environmental_monitor.py --endpoint a16860lqrlz0ti-ats.iot.us-east-1.amazonaws.com --rootCA AmazonRootCA1.pem --cert certificate.pem.crt --key private.pem.key --thingName RaspberryPi --clientId RaspberryPi
