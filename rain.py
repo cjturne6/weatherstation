007d6473a9341e290b51344f5814804402f9bebf
collin <turner.collin@gmail.com>
2021-04-25 15:24:03 +0100

    updated launch scripts, added functioning rain test.

---
+++ rain.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+
+BUTTON_GPIO = 26
+
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    while True:
+        GPIO.wait_for_edge(BUTTON_GPIO, GPIO.FALLING)
+        print("Button pressed!")
